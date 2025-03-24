b6e34264dd82fe04d76e06a01e8c04da6f11a6d2
emons <emons@SAFAYET>
2025-03-25 00:34:00 +0600

    Add project files.

---
+++ Services/Catalog/Catalog.Core/Entities/ProductBrands.cs
@@ -0,0 +1,10 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+
+namespace Catalog.Core.Entities
+{
+    public class ProductBrands : BaseEntity
+    {
+        [BsonElement("Name")]
+        public string Name { get; set; }
+    }
+}
