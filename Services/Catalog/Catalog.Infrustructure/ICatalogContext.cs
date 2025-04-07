c41592238e32d09828fe9f1c85f12b0489328f08
safayet <emonsafayetbs@outlook.com>
2025-04-07 16:37:18 +0600

    Catalog Context implemented.

---
+++ Services/Catalog/Catalog.Infrustructure/ICatalogContext.cs
@@ -0,0 +1,13 @@
+﻿using Catalog.Core.Entities;
+using MongoDB.Driver;
+
+namespace Catalog.Infrustructure
+{
+    public interface ICatalogContext
+    {
+        IMongoCollection<Product> Products { get; }
+        IMongoCollection<ProductBrands>  Brands { get; }
+        IMongoCollection<ProductType>  Types { get; }
+
+    }
+}
