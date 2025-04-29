c5467c0ca7f8654cc1f69e0d12c18c2ce6d0bc18
safayetwithxnc <safayetwithxnc@gmail.com>
2025-04-29 20:53:59 +0600

    #Basket Microservice Using Clean Architechture.

---
+++ Services/Basket/Basket.API/Controllers/ApiController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Asp.Versioning;
+
+namespace Basket.API.Controllers
+{
+    [ApiVersion("1")]
+    [Route("api/v{version:apiVersion}/[controller]")]
+    [ApiController]
+    public class ApiController : ControllerBase
+    {
+
+    }
+}
