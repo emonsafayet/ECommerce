c350d3b51e422be3c875fe7389db228efeb00637
safayetwithxnc <safayetwithxnc@gmail.com>
2025-05-09 20:43:36 +0600

    Deloping Ordering feature.

---
+++ Services/Ordering/Ordering.Infrastructure/Data/OrderContext.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using Ordering.Core.Entities;
+
+namespace Ordering.Infrastructure.Data
+{
+    public class OrderContext : DbContext
+    {
+        public OrderContext(DbContextOptions<OrderContext> options):base(options)
+        {
+
+        }
+        public DbSet<Order> Orders { get; set; }
+
+        public override Task<int> SaveChangesAsync(bool acceptAllChangesOnSuccess, CancellationToken cancellationToken=default)
+        {
+            foreach (var entry in ChangeTracker.Entries<Order>())
+            {
+
+                switch (entry.State)
+                {
+                    case EntityState.Added:
+                        entry.Entity.CreatedDate = DateTime.Now;
+                        entry.Entity.CreatedBy = "Safayet"; //TODO: Replace with auth server
+                        break;
+                    case EntityState.Modified:
+                        entry.Entity.LastModifiedDate = DateTime.Now;
+                        entry.Entity.LastModifiedBy = "Safayet"; //TODO: Replace with auth server
+                        break;
+                } 
+            }
+            return base.SaveChangesAsync(acceptAllChangesOnSuccess, cancellationToken);
+        } 
+    }
+}
---
+++ Services/Ordering/Ordering.Infrastructure/Data/OrderContextSeed.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Extensions.Logging;
+using Ordering.Core.Entities;
+
+namespace Ordering.Infrastructure.Data
+{
+    public class OrderContextSeed
+    {
+        public static async Task SeedAsync(OrderContext orderContext, ILogger<OrderContextSeed> logger)
+        {
+            if (!orderContext.Orders.Any())
+            {
+                orderContext.Orders.AddRange(GetOrders());
+                await orderContext.SaveChangesAsync();
+                logger.LogInformation($"Ordering Database: {typeof(OrderContext).Name} seeded!!!");
+            }
+        }
+
+        private static IEnumerable<Order> GetOrders()
+        {
+            return new List<Order>
+            {
+                new()
+                {
+                    UserName = "emonsafayet",
+                    FirstName = "Safayet",
+                    LastName = "Hosain",
+                    EmailAddress = "emonsafayet@eCommerce.net",
+                    AddressLine = "Chittagong",
+                    Country = "Bangladesh",
+                    TotalPrice = 750,
+                    State = "CHA",
+                    ZipCode = "4202",
+
+                    CardName = "Visa",
+                    CardNumber = "1234567890123456",
+                    CreatedBy = "Safayet",
+                    Expiration = "12/25",
+                    Cvv = "123",
+                    PaymentMethod = 1,
+                    LastModifiedBy = "Safayet",
+                    LastModifiedDate = new DateTime(),
+                }
+            };
+        }
+    }
+}
